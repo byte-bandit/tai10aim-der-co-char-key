11eab104bdb8434aef74b0d3349193f30293d066
klausklapper@gmail.com <klausklapper@gmail.com@1ac39b8c-4d03-8841-a1cf-59df759c9b43>
2012-06-05 14:55:16 +0000

    Lost.Found

---
+++ Adventure_Prototype/Adventure_Prototype/Adventure_Prototype/Classes/Net/Gamer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Lidgren.Network;
+
+namespace Classes.Net
+{
+	class Gamer
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public string Name { get; set; }
+		public bool ready { get; set; }
+		public NetConnection Connection { get; set; }
+		public Gamer(string name)
+		{
+			Name = name;
+		}
+	}
+}
